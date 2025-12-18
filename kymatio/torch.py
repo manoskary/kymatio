f31b5a45cf2a2f828e4580a93abec6d8c1dd69c0
manoskary <manoskaristineos@gmail.com>
2025-12-18 22:44:12 +0100

    Expose TimeFrequencyScattering1D alias

--- kymatio/torch.py
+++ kymatio/torch.py
@@ -13,6 +13,10 @@ Scattering1D.__name__ = "Scattering1D"
 TimeFrequencyScattering.__module__ = "kymatio.torch"
 TimeFrequencyScattering.__name__ = "TimeFrequencyScattering"
 
+TimeFrequencyScattering1D = TimeFrequencyScattering
+TimeFrequencyScattering1D.__module__ = "kymatio.torch"
+TimeFrequencyScattering1D.__name__ = "TimeFrequencyScattering1D"
+
 Scattering2D.__module__ = "kymatio.torch"
 Scattering2D.__name__ = "Scattering2D"
 
@@ -22,6 +26,7 @@ HarmonicScattering3D.__name__ = "HarmonicScattering3D"
 __all__ = [
     "Scattering1D",
     "TimeFrequencyScattering",
+    "TimeFrequencyScattering1D",
     "Scattering2D",
     "HarmonicScattering3D",
 ]
